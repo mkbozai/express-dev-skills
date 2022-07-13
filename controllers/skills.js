514c1732f81a661021222e597074eb9b0873d432
mkbozai <munaaf24@gmail.com>
2022-07-13 01:00:50 -0500

    Finish show functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,16 @@
 const Skill = require('../models/skill');
 
 module.exports = {
-    index
+    index,
+    show
 };
 
 function index(req, res) {
     const skills = Skill.getAll();
     res.render('skills/index', { skills })
+};
+
+function show(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/show', { skill });
 };
--- routes/skills.js
+++ routes/skills.js
@@ -4,5 +4,6 @@ const skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
